42aca7d3d531988caa7d789e920e837b9eed8508
Elen <elen.ghazaryan6@gmail.com>
2021-12-08 01:12:42 +0400

    feat: function that implements partial sum

---
+++ t1_partialSum.js
@@ -0,0 +1,15 @@
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+const addOne = sum(1);
+
+console.log(addOne(2));
+console.log(addOne(4));
+
+const addTen = sum(10);
+
+console.log(addTen(2));
+console.log(addTen(10));
